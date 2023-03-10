88d1e21cb75feacb7ce2f2c44275688477680c06
Vũ Phạm Tiến Dũng <43530592+vuphamtiendung@users.noreply.github.com>
2023-03-10 20:07:19 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DotnetCoreForms.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+
+namespace DotnetCoreForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            // TODO: gán bất kì giá trị mặc dịnh nào & gủi lại cho view
+            var model = new ProductEditModel(); 
+            return View(model);
+        }
+        
+        [HttpPost]
+        public IActionResult Create(ProductEditModel editModel)
+        {
+            string message = string.Empty;
+            if (ModelState.IsValid)
+            {
+                message = "Product: " + editModel.Name + ". Rate = " + editModel.Rate + ". Rating = " + editModel.Rating;
+            }
+            else
+            {
+                message = "Failed to create the product. Please try again";
+            }
+            return Content(message);
+        }
+    }
+}
---
+++ Models/ProductEditModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetCoreForms.Models
+{
+    public class ProductEditModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public decimal Rate { get; set; }   
+        public int Rating { get; set; }
+    }
+}
