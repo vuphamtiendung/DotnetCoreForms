87531a86d9c9236ae7077d4050b75bdbca6fe485
Vũ Phạm Tiến Dũng <43530592+vuphamtiendung@users.noreply.github.com>
2023-03-11 20:56:14 +0700

    Update ProductEditModel.cs

--- Models/ProductEditModel.cs
+++ Models/ProductEditModel.cs
@@ -1,10 +1,15 @@
-﻿namespace DotnetCoreForms.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace DotnetCoreForms_004.Models
 {
     public class ProductEditModel
     {
+        
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(maximumLength: 100, MinimumLength = 5, ErrorMessage = "Length must be between 5 to 100")]
         public string Name { get; set; }    
-        public decimal Rate { get; set; }   
-        public int Rating { get; set; }
+        public double Rate { get; set; }
+        public int Rating { get; set; } 
     }
 }
